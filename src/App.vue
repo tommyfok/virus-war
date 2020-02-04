cf9c7f4622a5574e87235ab39c79ca69ad4d7a21
tommyfok <154274174@qq.com>
2020-02-04 17:30:36 +0800

    update style and add some params

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,38 @@
   <div id="container">
     <div id="virusmap"></div>
     <div id="dayinfo">第{{day}}天</div>
-    <ul id="tuli">
+    <ul id="info">
       <li style="color:white">未感染</li>
       <li style="color:yellow">潜伏期</li>
       <li style="color:red">已确诊</li>
+      <li style="color:white">空余床位数：{{freeBedNum}}</li>
+      <li style="color:white">死亡人数：{{deadCount}}</li>
+      <li style="color:green">康复人数：{{healCount}}</li>
     </ul>
+    <div id="form" :class="{expand:showForm}">
+      <a class="btn btn-open" v-if="!showForm" @click="toggleForm">+ 调整参数</a>
+      <a class="btn btn-close" v-if="showForm" @click="toggleForm">- 调整参数</a>
+      <div class="form-content" v-if="showForm">
+        <div>人员活动范围：<input v-model="人员活动范围" type="number"></div>
+        <button class="confirm" @click="init">重新开始</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import * as PIXI from 'pixi.js'
-import { clearInterval } from 'timers';
-const 人员活动范围 = 10
-const 每天活动人数比例 = 0.02
-const 感染率 = 0.05
-const 样本数 = 20000
-const 初始感染数 = 10
-const 分块尺寸 = 5
+const 人员活动范围 = 20
+const 每天活动人数比例 = 0.1
+const 同区域感染率 = 0.005
+const 样本数 = 10000
+const 初始感染数 = 50
+const 分块尺寸 = 10
 const 潜伏期 = 14
-const 自愈概率 = 0.5
-const 治疗康复概率 = 0.8
-const 治疗周期 = 20
-const 床位数 = 样本数 / 1000
+const 自愈概率 = 0.2
+const 治疗康复概率 = 0.79
+const 最大治疗天数 = 30
+const 床位数 = 20
 
 let pixi = null
 let points = []
@@ -84,8 +94,13 @@ export default {
   name: 'app',
   data: () => {
     return {
+      showForm: false,
       day: 1,
-      startTime: 0,
+      dayTimer: null,
+      deadCount: 0,
+      healCount: 0,
+      人员活动范围,
+      freeBedNum: 床位数,
       configs: {
         totalCount: 样本数
       }
@@ -112,8 +127,14 @@ export default {
       if (pixi) {
         pixi.destroy()
       }
+      // 初始化数据
       points = []
-      this.startTime = Date.now()
+      this.day = 1
+      this.freeBedNum = 床位数
+      this.deadCount = 0
+      this.healCount = 0
+      this.人员活动范围 = this.人员活动范围 || 人员活动范围
+      // 创建舞台实例
       pixi = new PIXI.Application({
         resizeTo: document.getElementById('virusmap')
       })
@@ -136,9 +157,60 @@ export default {
       }
       this.dayTimer = setInterval(() => {
         this.day++
-        this.updateInfect()
+        this.updateHealed()
+        this.updateDead()
         this.updateQueZhen()
+        this.updateInfect()
       }, 1000)
+    },
+    heal(p) {
+      if (p.infectStatus !== 2) {
+        return
+      }
+      if (p.__inHospital === true) {
+        this.freeBedNum++
+      }
+      let newp = this.createPoint(0x00ff00)
+      newp.infectStatus = 3
+      newp.x = p.x
+      newp.y = p.y
+      newp.__xindex = p.__xindex
+      newp.__yindex = p.__yindex
+      let pIndex = points.indexOf(p)
+      points[pIndex] = newp
+      p.destroy()
+      pixi.stage.addChild(newp)
+      this.healCount++
+    },
+    dead(p) {
+      if (p.infectStatus !== 2) {
+        return
+      }
+      let newp = this.createPoint(0x333333)
+      newp.infectStatus = 4
+      newp.x = p.x
+      newp.y = p.y
+      newp.__xindex = p.__xindex
+      newp.__yindex = p.__yindex
+      let pIndex = points.indexOf(p)
+      points[pIndex] = newp
+      p.destroy()
+      pixi.stage.addChild(newp)
+      this.deadCount++
+    },
+    updateHealed() {
+      points.forEach(p => {
+        if (p.__healDay === this.day) {
+          this.heal(p)
+        }
+      })
+    },
+    updateDead() {
+      points.forEach(p => {
+        if (p.__deadDay === this.day) {
+          this.dead(p)
+        }
+      })
     },
     updateInfect() {
       let maxXIndex = Math.floor(pixi.renderer.width / 分块尺寸)
@@ -156,9 +228,8 @@ export default {
         let quezhenPoints = _points.filter(item => item.infectStatus === 2)
         let badCount = infectedPoints.length + quezhenPoints.length
         if (badCount > 0) {
-          let infectRate = badCount / _points.length
           healthyPoints.forEach(hp => {
-            let isInfect = Math.random() < 感染率
+            let isInfect = Math.random() < 同区域感染率
             if (isInfect) {
               this.infect(hp)
             }
@@ -177,9 +248,13 @@ export default {
       getRandItems(points, count).forEach(this.infect)
     },
     goAround(p) {
+      if (p.infectStatus === 4) {
+        // dead
+        return
+      }
       if (Math.random() < 每天活动人数比例) {
-        p.x += gaussianRand() * 人员活动范围 - 人员活动范围 / 2
-        p.y += gaussianRand() * 人员活动范围 - 人员活动范围 / 2
+        p.x += gaussianRand() * this.人员活动范围 - this.人员活动范围 / 2
+        p.y += gaussianRand() * this.人员活动范围 - this.人员活动范围 / 2
         p.__xindex = Math.floor(p.x / 分块尺寸)
         p.__yindex = Math.floor(p.y / 分块尺寸)
       }
@@ -209,10 +284,35 @@ export default {
       newp.infectStatus = 2
       newp.x = p.x
       newp.y = p.y
+      // 确诊了，看看是否有床位？
+      if (this.freeBedNum > 0) {
+        // 有床位
+        this.freeBedNum--
+        let willGood = Math.random() < 治疗康复概率
+        let outDay = this.day + Math.ceil(最大治疗天数 * gaussianRand())
+        newp.__inHospital = true
+        if (willGood) {
+          newp.__healDay = outDay
+        } else {
+          newp.__deadDay = outDay
+        }
+      } else {
+        // 没有床位，只能自求多福
+        let willGood = Math.random() < 自愈概率
+        let outDay = this.day + Math.ceil(最大治疗天数 * gaussianRand())
+        if (willGood) {
+          newp.__healDay = outDay
+        } else {
+          newp.__deadDay = outDay
+        }
+      }
       let pIndex = points.indexOf(p)
       points[pIndex] = newp
       p.destroy()
       pixi.stage.addChild(newp)
+    },
+    toggleForm() {
+      this.showForm = !this.showForm
     }
   }
 }
@@ -232,9 +332,10 @@ html, body {
   position: fixed;
   background: rgba(0,0,0,.5);
   color:#fff;
-  bottom: 10px;
-  right: 10px;
-  padding: 0 10px;
+  top: 10px;
+  left: 0;
+  right: 0;
+  text-align: center;
   border-radius: 3px;
   font-size: 20px;
   line-height: 40px;
@@ -249,9 +350,9 @@ html, body {
     transform: translateY(50%);
   }
 }
-#tuli {
+#info {
   position: fixed;
-  bottom: 10px;
+  bottom: 60px;
   left: 10px;
   font-size: 12px;
   line-height: 18px;
@@ -259,5 +360,31 @@ html, body {
   padding: 10px 10px 10px 25px;
   margin: 0;
   border-radius: 5px;
+}
+#form {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  color: #fff;
+  padding: 10px 20px;
+  width: 100%;
+  box-sizing: border-box;
+  font-size: 12px;
+  z-index: 100;
+  line-height: 40px;
+  transition: all .3s ease-out;
+
+  &.expand {
+    background: rgba(85, 115, 252, 0.836);
+  }
+
+  .btn {
+    font-size: 16px;
+    line-height: 40px;
+  }
+
+  .confirm {
+    background: #fff;
+  }
 }
 </style>
